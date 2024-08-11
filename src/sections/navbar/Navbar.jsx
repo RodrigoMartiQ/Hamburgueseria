d12d175a4608195fc97f8ea530029b11979536b1
RodrigoMartiQ <rodrigomarti2015@gmail.com>
2024-08-11 16:10:59 -0300

    mejoras al codigo

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -2,31 +2,36 @@ import style from "./style.module.css";
 
 function Navbar() {
   return (
-    
-    <header id="navb" className={style.Navbar} >
-      <div className={style.container}>
+    <header className={style.Navbar}>
+      <div id="navb" className={style.container}>
         <div className={style.navLimiter}>
           <div className={style.Leftside}>
             <h1>Samurai</h1>
           </div>
           <div className={style.Rightside}>
+            <button className={style.abrirmenu}>abrir</button>
 
-            <button id="abrir" className={style.abrirmenu}>abrir</button>
-
-            <nav className={style.nav} id="nav">
-
-              <button id="cerrar" className={style.cerrarmenu}>cerrar</button>
+            <nav className={style.nav}>
+              <button className={style.cerrarmenu}>cerrar</button>
 
               <ul className={style.navlist}>
-              <li><a className={style.links} href="#inicio">Inicio</a></li>
-              <li><a className={style.links} href="#menu">Menu</a></li>
-              <li><a className={style.links} href="#contactanos">Contactanos</a></li>
+                <li>
+                  <a className={style.links} href="#inicio">
+                    Inicio
+                  </a>
+                </li>
+                <li>
+                  <a className={style.links} href="#menu">
+                    Menu
+                  </a>
+                </li>
+                <li>
+                  <a className={style.links} href="#contactanos">
+                    Contactanos
+                  </a>
+                </li>
               </ul>
-              
             </nav>
-
-
-
           </div>
         </div>
       </div>
